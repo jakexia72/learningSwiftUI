8e701cc588389a0eec31924cbaba4f8d1e3a47e7
jakexia72 <jakexia72@hotmail.com>
2019-06-09 18:20:30 -0400

    first commit

---
+++ learningSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  learningSwiftUI
+//
+//  Created by Jake Xia on 2019-06-09.
+//  Copyright © 2019 Jake Xia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
